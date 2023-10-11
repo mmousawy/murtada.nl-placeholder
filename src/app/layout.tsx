4c3b96988180226fa79050892066c40d446f27bc
Murtada al Mousawy <murtada.al.mousawy@gmail.com>
2023-10-11 03:11:37 +0200

    Update styling.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import '@/styles/globals.scss'
 import type { Metadata } from 'next'
+import Image from 'next/image';
 import { Inter } from 'next/font/google'
+import styles from '@/styles/page.module.scss'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,7 +18,26 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        {children}
+        <Image
+          className={styles.bg}
+          src="/bg3.webp"
+          alt="Background image"
+          width={1920}
+          height={1080}
+        />
+        <svg viewBox="0 0 24 24" preserveAspectRatio="none" width="1920" height="1080" className={styles.noise}>
+        <filter id='noiseFilter'>
+          <feTurbulence
+            type='fractalNoise'
+            baseFrequency='80'
+            numOctaves='100'
+            stitchTiles='stitch' />
+        </filter>
+        <rect width='100%' height='100%' filter='url(#noiseFilter)' />
+      </svg>
+      </body>
     </html>
   )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,9 @@
-import Image from 'next/image'
-import styles from '@/styles/page.module.scss'
+import Image from 'next/image';
+import styles from '@/styles/page.module.scss';
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      <div className={styles.heading}>
-
-      </div>
-
       <div className={styles.center}>
         <Image
           className={styles.photo}
@@ -21,24 +17,24 @@ export default function Home() {
           <h1 className={styles.title}><span className={styles.name}>Murtada al Mousawy</span><br/>Senior Web Developer</h1>
           <div className={styles.asideDetails}>
             <span className={ styles.availabilityStatus }><span className={styles.bullet}></span> Available for work</span>
-            <svg width="32" height="32" className={styles.dots}>
+            <svg width="48" height="32" className={styles.dots}>
             <linearGradient id="gradient1">
             <stop id="stop1" offset="0%" stopColor="#dddddd"/>
             <stop id="stop2" offset="100%" stopColor="#222527"/>
             </linearGradient>
-            <rect x="0" y="15" width="32" height="2" fill="url(#gradient1)"/>
+            <rect x="0" y="15" width="48" height="2" fill="url(#gradient1)"/>
               <path
-                d="M 0 16 L 32 16"
+                d="M 0 16 L 48 16"
                 strokeMiterlimit="10"
                 fill="none"
                 stroke="#fff"
-                strokeWidth="2"
-                strokeDasharray="2"
-                strokeDashoffset="1">
+                strokeWidth="4"
+                strokeDasharray="4"
+                strokeDashoffset="0">
                 <animate
                   attributeName="stroke-dashoffset"
-                  values="20;0"
-                  dur="3s"
+                  values="48;0"
+                  dur="5s"
                   calcMode="linear"
                   repeatCount="indefinite"
                 />
