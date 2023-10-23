bc5b1650499c718ea5262044d4cd979bb22db64c
Murtada al Mousawy <murtada.al.mousawy@gmail.com>
2023-10-24 01:52:33 +0200

    Update app.

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,3 @@
-import withPlaiceholder from "@plaiceholder/next";
-
 /** @type {import('next').NextConfig} */
 import path from 'path';
 import { fileURLToPath } from 'url';
@@ -12,4 +10,4 @@ const nextConfig = {
   },
 };
 
-export default withPlaiceholder(nextConfig);
+export default nextConfig;
